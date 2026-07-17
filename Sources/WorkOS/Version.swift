b95b396c3b7bd933966145df67ef60e9cbb779b4
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2026-07-17 11:31:11 -0400

    a release, of sorts

---
+++ Sources/WorkOS/Version.swift
@@ -0,0 +1,7 @@
+// @oagen-ignore-file
+
+/// Version information for the WorkOS Swift SDK.
+public enum WorkOSVersion {
+    /// The semantic version of this SDK release.
+    public static let current = "0.0.1"  // x-release-please-version
+}
